7b006a6c8e6a94ddd049b198969695b7e70a5189
Krish Shah <shahkrish2016@gmail.com>
2024-05-31 14:43:00 -0400

    set up code to run llm through entire directory of images and to print out results to a JSON file

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
 from pathlib import Path
-# from predict import Predictor
-
-# predictor = Predictor()
-
-# raw_image = Path("custom-images") / "biryani.png"
-# response = predictor.predict(
-#     image = raw_image,
-#     prompt = "What are the individual ingredents in the food?" 
-# )
-
-# print(response)
-
-# for text in response:
-#     print(text)
-
 import subprocess
+import json
+from tqdm import tqdm
+import time
 
-def get_response(image_file, questions, verbose=False):
+# TODO: implement verbose feature
+def get_response(image_file: Path, 
+                 questions: list[str]):
+    if not image_file.exists():
+        raise ValueError(f"No image exists at {image_file.absolute()}")
+
     model_path = "liuhaotian/llava-v1.5-7b"
     command = [
-        "python3", "-m", "llava.serve.cli",
+        "python3", "-W ignore", "-m",  
+        "llava.serve.cli",
         "--model-path", model_path,
         "--image-file", image_file
     ]
@@ -28,14 +22,12 @@ def get_response(image_file, questions, verbose=False):
     
     answers = {}
 
-    answers['image_file'] = image_file
+    answers['image_path'] = str(image_file)
+    answers['questions'] = []
 
     for question in questions:
-        answers[question] = ''
+        result = subprocess.run(command, input=question, stdout=subprocess.PIPE, text=True)
 
-        # if verbose, then have subprocess.run instead of subprocess.Popen
-        result = subprocess.Popen(command, input=question, stdout=subprocess.PIPE, text=True)
-        
         result = result.stdout
 
         # Split the string based on "ASSISTANT:"
@@ -50,22 +42,34 @@ def get_response(image_file, questions, verbose=False):
             actual_response = actual_response[:user_index]
 
         actual_response = actual_response.strip()
-        print(f"'{actual_response}'")
+        # print(f"'{actual_response}'")
 
-        answers[question] = actual_response
+        answers['questions'].append({'prompt': question, 'answer': actual_response})
     
     return answers
 
-image_file = Path("custom-images") / "bread.png"
-# get_response(image_file, question="Provide a detailed description of the food you see in the image.")
 
-questions = [
-    "1) Provide a detailed description of the food you see in the image.",
-    "2) Provide a detailed list of the ingredients of the food in the image.",
-    "3) Provide an approximate estimate the weight of the food in the image in grams. It is completely okay if your estimate is off, all I care about is getting an estimate."
-]
+if __name__ == "__main__":
+    images_folder = Path("custom-images")
 
-get_response(image_file, questions)
+    questions = [
+        "Provide a detailed description of the food you see in the image.",
+        "Provide a detailed list of the ingredients of the food in the image.",
+        "Provide an approximate estimate the weight of the food in the image in grams. It is completely okay if your estimate is off, all I care about is getting an estimate."
+    ]
 
-# for question in questions:
-#     get_response(image_file, question)
+    start = time.time()    
+
+    answers = []
+    for img in tqdm(images_folder.iterdir()):
+        print(f"\n\nprocessing {img.name}...\n")
+        if img.suffix in ['.png', '.jpg']:
+            answers.append(get_response(img, questions))
+
+        # keep updating answers in case of a crash
+        with open('data.json', 'w') as f:
+            json.dump(answers, f, indent=4)
+
+    end = time.time()    
+    print(f"{round(end - start, 2)} seconds elapsed...")
+
