472fbad55d9ff044a8753f8ae336405fb85266de
Krish Shah <shahkrish2016@gmail.com>
2024-06-19 14:35:45 -0400

    deleted all openpose code

--- infer.py
+++ infer.py
@@ -6,11 +6,9 @@ from PIL import Image
 
 from utils import *
 from dwpose import DWposeDetector
-from openpose import OpenposeDetector
 
 
 if __name__ == '__main__' :
-    
     image_dirs = ["./assets/test.jpeg", "./assets/test1.jpeg", "./assets/test2.jpeg", "./assets/test3.jpeg"]
     
     det_config = './dwpose/yolox_config/yolox_l_8xb8-300e_coco.py'
@@ -22,26 +20,18 @@ if __name__ == '__main__' :
     dwprocessor = DWposeDetector(det_config, det_ckpt, pose_config, pose_ckpt, device)
     
     annotator_ckpts_path = "./ckpts"
-    preprocessor = OpenposeDetector(annotator_ckpts_path)
 
     save_dir = "./outputs"
     os.makedirs(save_dir, exist_ok=True)
 
     for image_dir in image_dirs:
-
         input_image = cv2.imread(image_dir)
         H, W, C = input_image.shape
         input_image = HWC3(input_image)
         input_image = resize_image(input_image, resolution=512)
-        
-        detected_map_openpose = preprocessor(input_image, hand_and_face=True)
-        detected_map_openpose = HWC3(detected_map_openpose)
         
         detected_map = dwprocessor(input_image)
         detected_map = HWC3(detected_map)
         
         detected_map = cv2.resize(detected_map, (W, H), interpolation=cv2.INTER_LINEAR)
         cv2.imwrite(os.path.join(save_dir, image_dir.split('/')[-1]), detected_map)
-        
-        detected_map_openpose = cv2.resize(detected_map_openpose, (W, H), interpolation=cv2.INTER_LINEAR)
-        cv2.imwrite(os.path.join(save_dir, image_dir.split('/')[-1].split('.')[0]+"_openpose.jpg"), detected_map_openpose)
