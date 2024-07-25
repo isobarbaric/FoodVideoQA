271d1f6046a31d37b93917df6c042a932d04c3f6
Krish Shah <shahkrish2016@gmail.com>
2024-07-25 18:50:57 -0400

    parameterized what language model to use

--- llm/generation/llava.py
+++ llm/generation/llava.py
@@ -2,43 +2,68 @@ from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 import torch
 from PIL import Image
 from pathlib import Path
+from typing import Literal, get_args
 
 # using LLaVA-v1.6
-processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
-model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf", torch_dtype=torch.float16, low_cpu_mem_usage=True) 
-model.generation_config.pad_token_id = model.generation_config.eos_token_id
+models = Literal["llava-hf/llava-v1.6-mistral-7b-hf"]
+SUPPORTED_MODELS = get_args(models)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
+def get_model(model_name: str):
+  if model_name not in SUPPORTED_MODELS:
+    raise ValueError(f"{model_name} model not supported; supported models are {SUPPORTED_MODELS}")
+
+  processor, model = None, None
+  match model_name:
+    case "llava-hf/llava-v1.6-mistral-7b-hf":
+      processor = LlavaNextProcessor.from_pretrained(model_name)
+      model = LlavaNextForConditionalGeneration.from_pretrained(model_name, torch_dtype=torch.float16) 
+
+  model.generation_config.pad_token_id = model.generation_config.eos_token_id
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  model.to(device)
+  return processor, model
+
 
 # TODO: turn prompt into prompts
 # TODO: get Cache error to go away
-def get_response(prompt: str, img_path: Path):
-  image = Image.open(img_path)
-  # each value in "content" has to be a list of dicts with types ("text", "image")
-  conversation = [
-      {
-        "role": "user",
-        "content": [
-            {"type": "text", "text": prompt},
-            {"type": "image"},
-          ],
-      }
-  ]
-  rev_prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-  inputs = processor(rev_prompt, image, return_tensors="pt").to("cuda")
+def make_get_response(model_name: str):
+  processor, model = get_model(model_name)
 
-  # the .generate() function is the origin of the `past_key_values` bug
-  output = model.generate(**inputs, max_new_tokens=100)
-  model_response = processor.decode(output[0], skip_special_tokens=True) 
+  def get_response(prompt: str, img_path: Path):
+    image = Image.open(img_path)
+    # each value in "content" has to be a list of dicts with types ("text", "image")
+    conversation = [
+        {
+          "role": "user",
+          "content": [
+              {"type": "text", "text": prompt},
+              {"type": "image"},
+            ],
+        }
+    ]
+    rev_prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
-  actual_response = model_response.split('[/INST]')[1].strip()
-  return actual_response
+    print(f"with generation prompt: {processor.apply_chat_template(conversation, add_generation_prompt=True)}")
+    print(f"with generation prompt: {processor.apply_chat_template(conversation)}")
+
+    inputs = processor(rev_prompt, image, return_tensors="pt").to("cuda")
+
+    # the .generate() function is the origin of the `past_key_values` bug
+    output = model.generate(**inputs, max_new_tokens=100)
+    model_response = processor.decode(output[0], skip_special_tokens=True) 
+
+    actual_response = model_response.split('[/INST]')[1].strip()
+    return actual_response
+  
+  return get_response
 
 
 if __name__ == "__main__":
   prompt = "What is shown in this image?"
   img_path = Path("eat.jpg")
-  
+
+  model_name = "llava-hf/llava-v1.6-mistral-7b-hf" 
+  get_response = make_get_response(model_name)
+
   response = get_response(prompt, img_path)
   print(response)
