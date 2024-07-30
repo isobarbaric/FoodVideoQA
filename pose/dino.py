8f879401c5c55a30db79e731572df89c19841346
Krish Shah <shahkrish2016@gmail.com>
2024-07-30 17:24:23 -0400

    created a dataclass for bounding boxes (DRY)

--- pose/dino.py
+++ pose/dino.py
@@ -7,6 +7,7 @@ from typing import Literal, get_args
 import cv2
 from PIL import Image
 import numpy as np
+from dataclasses import dataclass
 
 ROOT = Path(__file__).parent.parent
 IMAGE_DIR = ROOT / "data" / "llm" / "misc-images"
@@ -65,7 +66,7 @@ def make_get_bounding_boxes(model_name: str):
     """
     processor, model, device = get_model(model_name)
 
-    def generate_bounding_boxes(labels: list, image_path: Path):
+    def generate_bounding_boxes(labels: list, image_path: Path) -> BoundingBox:
         """
         Generate bounding boxes for the given labels in the image.
 
@@ -90,7 +91,9 @@ def make_get_bounding_boxes(model_name: str):
             target_sizes=[image.size[::-1]]
         )
 
-        return results[0]
+        bboxes_data = results[0]
+        # pprint.pprint(bboxes_data)
+        return process_bounding_boxes(bboxes_data)
     
     return generate_bounding_boxes
 
@@ -130,9 +133,10 @@ def draw_text(
     return text_size
 
 
+# TODO: refactor this to use list[DINOBoundingBox]
 def draw_bounding_boxes(
     image_path: Path, 
-    bounding_boxes: dict, 
+    bounding_boxes_data: dict, 
     output_path: Path,
     show: bool = False
 ):
@@ -147,9 +151,9 @@ def draw_bounding_boxes(
     """
     image = cv2.imread(str(image_path))
 
-    scores = bounding_boxes['scores'].cpu().numpy()
-    labels = bounding_boxes['labels']
-    boxes = bounding_boxes['boxes'].cpu().numpy()
+    scores = bounding_boxes_data['scores'].cpu().numpy()
+    labels = bounding_boxes_data['labels']
+    boxes = bounding_boxes_data['boxes'].cpu().numpy()
 
     for score, label, (xmin, ymin, xmax, ymax) in zip(scores, labels, boxes):
         img_label = f'{label}: {score:0.2f}'
@@ -174,37 +178,64 @@ def draw_bounding_boxes(
         cv2.destroyAllWindows()
 
 
-def get_mouth_bbox(bounding_boxes: dict) -> list:
-    scores = bounding_boxes['scores'].cpu().numpy()
-    labels = bounding_boxes['labels']
-    boxes = bounding_boxes['boxes'].cpu().numpy()
+@dataclass
+class BoundingBox:
+    bounding_box: np.ndarray
+    label: str
+    score: float
 
-    mouth_bboxes = []
-    for scores, label, bbox in zip(scores, labels, boxes):
-        if label == 'mouth':
-            mouth_bboxes.append(bbox)
+    @property
+    def xmin(self) -> float:
+        return self.bounding_box[0]
+
+    @property
+    def ymin(self) -> float:
+        return self.bounding_box[1]
+
+    @property
+    def xmax(self) -> float:
+        return self.bounding_box[2]
+
+    @property
+    def ymax(self) -> float:
+        return self.bounding_box[3]
+
+    @property
+    def center(self) -> tuple[int, int]:
+        xmin, ymin, xmax, ymax = self.bounding_box
+        x_mid = (xmin + xmax) / 2
+        y_mid = (ymin + ymax) / 2
+        return x_mid, y_mid
+
+
+def process_bounding_boxes(bounding_boxes_data: dict) -> list[BoundingBox]:
+    scores = bounding_boxes_data['scores'].cpu().numpy()
+    labels = bounding_boxes_data['labels']
+    boxes = bounding_boxes_data['boxes'].cpu().numpy()
+
+    data = []
+    for score, label, bbox in zip(scores, labels, boxes):
+        data.append(BoundingBox(bbox, label, score))
+
+    return data
+
+
+def get_mouth_bbox(bounding_boxes: list[BoundingBox]):
+    bboxes = [bbox_obj for bbox_obj in bounding_boxes if bbox_obj.label == 'mouth']
     
-    if len(mouth_bboxes) == 0:
+    if len(bboxes) == 0:
         raise IndexError(f"No bounding box found associated with label 'mouth'")
 
-    if len(mouth_bboxes) > 1:
+    if len(bboxes) > 1:
         raise ValueError(f"More than one bounding box found with label 'mouth'")
 
-    return mouth_bboxes[0]
+    return bboxes[0]
 
 
-def get_food_bboxes(bounding_boxes) -> list[list]:
-    scores = bounding_boxes['scores'].cpu().numpy()
-    labels = bounding_boxes['labels']
-    boxes = bounding_boxes['boxes'].cpu().numpy()
+def get_food_bboxes(bounding_boxes: list[BoundingBox]):
+    bboxes = [bbox_obj for bbox_obj in bounding_boxes if bbox_obj.label == 'food']
+    return bboxes
 
-    food_bboxes = []
-    for scores, label, bbox in zip(scores, labels, boxes):
-        if label == 'food':
-            food_bboxes.append(bbox)
-
-    return food_bboxes
-    
 
 if __name__ == "__main__":
     model_name = "IDEA-Research/grounding-dino-base"
@@ -215,11 +246,15 @@ if __name__ == "__main__":
 
     labels = ["food", "mouth"]
     bounding_boxes = generate_bounding_boxes(labels, image_path)
+    print("all bounding boxes:")
     pprint.pprint(bounding_boxes)
+
     # draw_bounding_boxes(image_path, bounding_boxes, output_path)
 
     mouth_bbox = get_mouth_bbox(bounding_boxes)
+    print("\nmouth bounding box:")
     pprint.pprint(mouth_bbox)
 
     food_bboxes = get_food_bboxes(bounding_boxes)
+    print("\nfood bounding boxes:")
     pprint.pprint(food_bboxes)
