560a77510ff90d449124faf3a38fe369842c91f0
Krish Shah <shahkrish2016@gmail.com>
2024-08-08 23:24:48 -0400

    refactored to include reference to `Labels` and implemented intersection method

--- pose/localization/bbox_utils.py
+++ pose/localization/bbox_utils.py
@@ -1,4 +1,4 @@
-from pose.localization.bbox import BoundingBox
+from pose.localization.bbox import BoundingBox, Labels
 import numpy as np
 from shapely.geometry import Polygon
 
@@ -9,7 +9,7 @@ def distance(bbox1: BoundingBox, bbox2: BoundingBox) -> float:
 
 
 def get_food_bboxes(bounding_boxes: list[BoundingBox]):
-    bboxes = [bbox_obj for bbox_obj in bounding_boxes if bbox_obj.label == 'food']
+    bboxes = [bbox_obj for bbox_obj in bounding_boxes if bbox_obj.label == Labels.FOOD]
     return bboxes
 
 
@@ -24,13 +24,13 @@ def get_closest_food_bbox(mouth_bbox: BoundingBox, food_bboxes: list[BoundingBox
 
 
 def get_mouth_bbox(bounding_boxes: list[BoundingBox]):
-    bboxes = [bbox_obj for bbox_obj in bounding_boxes if bbox_obj.label == 'mouth']
+    bboxes = [bbox_obj for bbox_obj in bounding_boxes if bbox_obj.label == Labels.MOUTH]
     
     if len(bboxes) == 0:
-        raise IndexError(f"No bounding box found associated with label 'mouth'")
+        raise IndexError(f"No bounding box found associated with label '{Labels.MOUTH}'")
 
     if len(bboxes) > 1:
-        raise ValueError(f"More than one bounding box found with label 'mouth'")
+        raise ValueError(f"More than one bounding box found with label '{Labels.MOUTH}'")
 
     return bboxes[0]
 
@@ -44,22 +44,21 @@ def bbox_intersection(bbox1: BoundingBox, bbox2: BoundingBox) -> float:
     union = bbox1.area + bbox2.area
     print(f"bbox1: {bbox1.area}, bbox2: {bbox2.area}, union: {union}")
 
-    bbox1_poly = Polgyon([
+    bbox1_ = Polygon([
         (bbox1.xmin, bbox1.ymin),
         (bbox1.xmax, bbox1.ymin),
         (bbox1.xmax, bbox1.ymax),
         (bbox1.xmin, bbox1.ymax)
     ])
-    bbox2_poly = Polgyon([
+    bbox2_ = Polygon([
         (bbox2.xmin, bbox2.ymin),
         (bbox2.xmax, bbox2.ymin),
         (bbox2.xmax, bbox2.ymax),
         (bbox2.xmin, bbox2.ymax)
     ])
-    intersection = bbox1_poly.intersection(bbox2_poly)
 
-    # intersection = np.lVkkkkkk
-    # union = np.logical_or(bbox1, bbox2)
-    # iou_score = np.sum(intersection) / np.sum(union)
-    # print(f"IoU is: {iou_score}")
+    intersection = bbox1_.intersection(bbox2_).area
+    union = bbox1_.union(bbox2_).area
+
+    return intersection / union
 
