4fa0864c1453a2a55df7ea602065f6a9c8f9ce8b
Krish Shah <shahkrish2016@gmail.com>
2024-10-15 21:22:18 -0400

    integrated `frame_diff` into `data.json` instead of being in a different file

--- llm/frame_diff/frames.py
+++ llm/frame_diff/frames.py
@@ -10,12 +10,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, T5Tokenizer, T5For
 from transformers import ByT5Tokenizer, BartForConditionalGeneration, BartTokenizer, BartModel
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
 
-ROOT_DIR = Path(__file__).parent.parent.parent
-DATA_DIR = ROOT_DIR / "data"
-LLM_DIR = DATA_DIR / "llm" 
-
-INPUT_PATH = LLM_DIR / "data.json"
-OUTPUT_PATH = LLM_DIR / "frame_diff_trial.json"
+from pprint import pprint
 
 # all LLMs similar supported by huggingface
 models = Literal[
@@ -88,36 +83,29 @@ def get_model(model_name: str):
                       'google-t5/t5-small', 'google-t5/t5-base', 'google-t5/t5-large', 'google-t5/t5-3b', 'google-t5/t5-11b']:
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = T5ForConditionalGeneration.from_pretrained(model_name)
-        model.to(device)
     elif model_name == 'CohereForAI/aya-101':
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = T5ForConditionalGeneration.from_pretrained(model_name)
-        model.to(device)
     elif model_name == 'Rostlab/prot_t5_xl_uniref50':
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModelForCausalLM.from_pretrained(model_name)
-        model.to(device)
     elif model_name == 'facebook/m2m100_418M':
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModelForCausalLM.from_pretrained(model_name)
-        model.to(device)
     elif model_name == 'meta-llama/Meta-Llama-3-8B-Instruct':
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModelForCausalLM.from_pretrained(model_name)
-        model.to(device)
     elif model_name == 'facebook/bart-base' or model_name == 'facebook/bart-large':
         tokenizer = BartTokenizer.from_pretrained(model_name)
         model = BartForConditionalGeneration.from_pretrained(model_name)
-        model.to(device)
     elif model_name == 'EleutherAI/gpt-neo-1.3B' or model_name == 'EleutherAI/gpt-neo-2.7B' or model_name == 'EleutherAI/gpt-j-6B':
         tokenizer = GPT2Tokenizer.from_pretrained(model_name, ignore_mismatched_sizes=True)
         model = GPTNeoForCausalLM.from_pretrained(model_name, ignore_mismatched_sizes=True)
-        model.to(device)
     else:
         model = DEFAULT_MODEL
         tokenizer = AutoTokenizer.from_pretrained(model)
         model = LlavaForConditionalGeneration.from_pretrained(model)
-        model.to(device)
+    model.to(device)
 
     model.config.pad_token_id = model.config.eos_token_id
 
@@ -125,14 +113,14 @@ def get_model(model_name: str):
 
 
 
-def load_frames():
+def load_frames(data_path: Path):
     """
     Load and parse frames from a JSON file into a dictionary.
 
     Returns:
         dict: A dictionary where the keys are video names and the values are lists of frames. Each frame is a dictionary containing frame number and associated questions.
     """
-    with open(INPUT_PATH, 'r') as file:
+    with open(data_path, 'r') as file:
         data = json.load(file)
 
     frames_dict = {}
@@ -180,7 +168,19 @@ def convert_to_dict(response: str):
 
     return response_dict
 
-def generate_frame_diff(model_name: str = DEFAULT_MODEL):
+def load_data(data_path: Path):
+    """
+    Load and parse data from a JSON file.
+
+    Returns:
+        list: A list of dictionaries containing data for each video.
+    """
+    with open(data_path, 'r') as file:
+        data = json.load(file)
+
+    return data
+
+def generate_frame_diff(input_path: Path, output_path: Path, model_name: str = DEFAULT_MODEL, print_output: bool = False):
     """
     Generate a structured dictionary of differences between consecutive video frames, focusing on new and absent food items.
 
@@ -189,7 +189,8 @@ def generate_frame_diff(model_name: str = DEFAULT_MODEL):
     console = Console()
     tokenizer, model, device = get_model(model_name)
 
-    frames_dict = load_frames()
+    data_json = load_data(input_path)
+    frames_dict = load_frames(input_path)
     output_dict = {}
 
     def clean_response(response: str):
@@ -199,29 +200,26 @@ def generate_frame_diff(model_name: str = DEFAULT_MODEL):
     frame_diff_prompt =   """
                 You are analyzing changes between two consecutive frames in a video. Focus ONLY on the presence of NEW food items and the absence of OLD food items between the two descriptions.
 
-                Please provide your answer in the following structured dictionary format:
-
+                Please provide your answer in the following structured dictionary format. ONLY include edible food items in your response.
                 {
-                    "new": [List of food items that appear in the current frame but were not present in the previous frame],
-                    "absent": [List of food items that were present in the previous frame but are missing in the current frame]
+                    "new": [List of food items that appear in the current frame but were not present in the previous frame. INCLUDE ONLY EDIBLE PREPARED FOOD ITEMS.],
+                    "absent": [List of food items that were present in the previous frame but are missing in the current frame. INCLUDE ONLY EDIBLE PREPARED FOOD ITEMS.]
                 }
 
                 Only mention the food items that are NEW or ABSENT. If there are none in either list, leave the list empty. Do NOT include any other information in your response.
                 """
+    
+    for video in data_json:
+        video_name = video["video_name"]
+        frames = frames_dict[video_name]
 
-    for video_name, frames in frames_dict.items():
-        output_dict[video_name] = []
         for i in range(1, len(frames)):
             prev_frame = frames[i - 1]
             curr_frame = frames[i]
 
             prev_desc = "\n\n Previous Description: " + prev_frame["questions"][0]
             curr_desc = "\n\n Current Description: " + curr_frame["questions"][0]
-
             prompt = frame_diff_prompt + prev_desc + curr_desc
-            console.print(f"[yellow]PROMPT:[/yellow]")
-            console.print(f"[green]{prompt}[/green]")
-            print()
 
             model_prompt = f"USER: {prompt}\nASSISTANT:"
             inputs = tokenizer(text=model_prompt, return_tensors="pt")
@@ -229,23 +227,36 @@ def generate_frame_diff(model_name: str = DEFAULT_MODEL):
             output = model.generate(**inputs, max_new_tokens=256)
             response = tokenizer.decode(output[0], skip_special_tokens=True)
             response = clean_response(response)
+            
+            if print_output:
+                console.print(f"[yellow]PROMPT:[/yellow]")
+                console.print(f"[green]{prompt}[/green]")
+                print()
 
-            console.print(f"[yellow]RESPONSE:[/yellow]")
-            console.print(f"[blue]{response}[/blue]\n\n")
-            print()
+                console.print(f"[yellow]RESPONSE:[/yellow]")
+                console.print(f"[blue]{response}[/blue]\n\n")
+                print()
 
             response_dict = convert_to_dict(response)
 
-            output_dict[video_name].append({
-                "frame_number": curr_frame["frame_number"],
-                "difference": response_dict
-            })
+            # add to current video in data.json, and overwrite the file
+            video["frames"][i]["difference"] = response_dict
 
-    with open(OUTPUT_PATH, 'w') as outfile:
-        json.dump(output_dict, outfile, indent=4)
+            if video_name not in output_dict:
+                output_dict[video_name] = []
+            output_dict[video_name].append({"frame_number": curr_frame["frame_number"], "difference": response_dict})
+    
+    # sort each video's frames by frame number
+    for video in output_dict:
+        output_dict[video] = sorted(output_dict[video], key=lambda x: x["frame_number"])
+
+    with open(output_path, 'w') as file:
+        json.dump(data_json, file, indent=4)
+        
 
 
-def determine_eaten():
+
+def determine_eaten(data_json: Path, print_output: bool = False):
     """
     Analyze the output JSON file to determine which food items are missing in each frame compared to previous frames.
 
@@ -253,20 +264,41 @@ def determine_eaten():
     """
     console = Console()
 
-    with open(OUTPUT_PATH, 'r') as file:
+    with open(data_json, 'r') as file:
         data = json.load(file)
 
-    absent = []
-    for video_name, frames in data.items():
-        console.print(f"[yellow]Absent food items in {video_name}[/yellow]\n")
-        for frame in frames:
-            diff = frame["difference"]
-            if diff["absent"]:
-                console.print(f"[red]Frame Number: {frame['frame_number']}[/red]")
-                for item in diff["absent"]:
-                    print(f"- {item}")
-                    absent.append(item)
-                print()
+    for video in data:
+        if print_output:
+            console.print(f"[yellow]Video Name: {video['video_name']}[/yellow]")
+            console.print()
+
+        absent = []
+        for frame in video["frames"]:
+            frame_number = frame["frame_number"]
+
+            if "difference" not in frame:
+                frame["difference"] = {
+                    "new": [],
+                    "absent": []
+                }
+
+            difference = frame["difference"]
+
+            if difference["absent"]:
+
+                if print_output:
+                    console.print(f"[blue]Frame Number: {frame_number}[/blue]")
+                    console.print(f"[red]Absent Food Items:[/red]")
+                    for item in difference["absent"]:
+                        console.print(f"  - {item}")
+                    console.print()
+                
+                absent.extend(difference["absent"])
+        
+        video["eaten foods: "] = absent
+
+    with open(data_json, 'w') as file:
+        json.dump(data, file, indent=4)
 
 
 if __name__ == "__main__":
